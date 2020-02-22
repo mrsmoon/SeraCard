7d65209067a649df956732a6c578e74d22a0ac3a
Sera <dev.sera20@gmail.com>
2020-02-22 13:30:01 +0300

    Initial Commit

---
+++ SeraCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeraCard
+//
+//  Created by Sera on 2/22/20.
+//  Copyright © 2020 Sera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
